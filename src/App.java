5fa6f44607c1222d669a54cfe07a59b8c909cdb0
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2022-07-19 18:15:36 -0300

    troca da url e parâmetro start no rating do sticker

--- src/App.java
+++ src/App.java
@@ -11,8 +11,9 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // fazer conexão HTTP e buscar os top 250 movies
-        String API_KEY = "549a5d8b";
-        String url = "https://api.mocki.io/v2/" + API_KEY;
+        // String API_KEY = "549a5d8b";
+        // String url = "https://api.mocki.io/v2/" + API_KEY;
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
 
         URI uri = URI.create(url);
 
@@ -27,13 +28,17 @@ public class App {
 
         List<Map<String, String>> moviesList = parser.parse(bodyJSON);
 
+        // criar parametro start para o rating no stickers
+        int startRatingParameter = 360;
+
         // exibir e manipular os dados
         var generator = new StickerGenerator();
         for (Map<String, String> movie : moviesList) {
-            String imageURL = movie.get("image");
-                // .replaceAll(
-                //     "._V1_UX128_CR0,3,128,176_AL_"
-                //     , "._V1_UX800_CR0,3,800,1177_AL_");
+            String imageURL = movie.get("image")
+                .replaceAll(
+                    "._V1_UX128_CR0,3,128,176_AL_"
+                    , "._V1_UX800_CR0,3,800,1177_AL_");
+
             String title = movie.get("title");
             String fileName = "assets/outputs/" + title + " STICKER.png";
 
@@ -45,10 +50,13 @@ public class App {
             double ratingStars = Double.parseDouble(movie.get("imDbRating"));
             for (int i = 1; i <= ratingStars; i++) {
                 subtitle += "\u2B50";
+                startRatingParameter -= 30;
             }
             System.out.println(title + "\n" + imageURL + "\n\n");
 
-            generator.createSticker(inputStream, fileName, subtitle);
+            generator.createSticker(inputStream, fileName, subtitle, startRatingParameter);
+
+            startRatingParameter = 360;
         }
     }
 }
--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -9,7 +9,7 @@ import javax.imageio.ImageIO;
 
 public class StickerGenerator {
 
-  void createSticker(InputStream inputStream, String fileName, String subtitle) throws Exception {
+  void createSticker(InputStream inputStream, String fileName, String subtitle, int startRatingParameter) throws Exception {
     // leitura da imagem
     // InputStream inputStream = new FileInputStream(new File("assets/inputs/movie.jpg"));
     // InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BMzE5MDM1NDktY2I0OC00YWI5LTk2NzUtYjczNDczOWQxYjM0XkEyXkFqcGdeQXVyMTQxNzMzNDI@.jpg").openStream();
@@ -24,15 +24,16 @@ public class StickerGenerator {
 
     // copiar a imagem original pra nova imagem (em memória)
     Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+
     graphics.drawImage(originalImage, 0, 0, width, height, null);
 
     // configurar a fonte
-    var font = new Font(Font.SANS_SERIF, Font.BOLD, 78);
+    var font = new Font("Courrier", Font.PLAIN, 78);
     graphics.setColor(Color.YELLOW);
     graphics.setFont(font);
 
     // escrever uma frase na nova imagem
-    graphics.drawString(subtitle, 100, newHeight - 50);
+    graphics.drawString(subtitle, startRatingParameter, newHeight - 50);
 
     // escrever nova imagem num arquivo
     ImageIO.write(newImage, "png", new File(fileName));

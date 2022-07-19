aa1fdc7371a48373e92e4343a9514e8efff3b30d
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2022-07-19 17:10:44 -0300

    configurando sticker para receber um rating específico

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -8,39 +10,45 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // fazer conexão HTTP e buscar os top 250 filmes
+        // fazer conexão HTTP e buscar os top 250 movies
         String API_KEY = "549a5d8b";
         String url = "https://api.mocki.io/v2/" + API_KEY;
-        
+
         URI uri = URI.create(url);
 
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(uri).build();
 
-        String bodyJSON = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        String bodyJSON = client.send(request, HttpResponse.BodyHandlers
+            .ofString()).body();
 
-        // separar e extrair somente os dados que interessam, como título, avaliação, poster
+        // separar e extrair somente os dados que interessam
         var parser = new JSONParser();
 
         List<Map<String, String>> moviesList = parser.parse(bodyJSON);
 
-        //exibir e manipular os dados
-        for (Map<String,String> filme : moviesList) {
-            System.out.print("\u001b[1m Título: ");
-            System.out.println(filme.get("title"));
-            
-            System.out.print("\u001b[1m Pôster: ");
-            System.out.println(filme.get("image"));
+        // exibir e manipular os dados
+        var generator = new StickerGenerator();
+        for (Map<String, String> movie : moviesList) {
+            String imageURL = movie.get("image");
+                // .replaceAll(
+                //     "._V1_UX128_CR0,3,128,176_AL_"
+                //     , "._V1_UX800_CR0,3,800,1177_AL_");
+            String title = movie.get("title");
+            String fileName = "assets/outputs/" + title + " STICKER.png";
 
-            System.out.print("\u001b[1m \u001b[44;1m Classificação: ");
-            System.out.println("\u001b[46;1m " + filme.get("imDbRating"));
+            InputStream inputStream = new URL(imageURL)
+                    .openStream();
 
-            double ratingStars = Double.parseDouble(filme.get("imDbRating"));
-            for(int i=1; i<=ratingStars; i++){
-                System.out.print("\u2B50");
+            String subtitle = "";
+
+            double ratingStars = Double.parseDouble(movie.get("imDbRating"));
+            for (int i = 1; i <= ratingStars; i++) {
+                subtitle += "\u2B50";
             }
+            System.out.println(title + "\n" + imageURL + "\n\n");
 
-            System.out.println("\n");
+            generator.createSticker(inputStream, fileName, subtitle);
         }
     }
 }
---
+++ src/StickerGenerator.java
@@ -0,0 +1,42 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class StickerGenerator {
+
+  void createSticker(InputStream inputStream, String fileName, String subtitle) throws Exception {
+    // leitura da imagem
+    // InputStream inputStream = new FileInputStream(new File("assets/inputs/movie.jpg"));
+    // InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BMzE5MDM1NDktY2I0OC00YWI5LTk2NzUtYjczNDczOWQxYjM0XkEyXkFqcGdeQXVyMTQxNzMzNDI@.jpg").openStream();
+    BufferedImage originalImage = ImageIO.read(inputStream);
+
+    // cria nova imagem em memória com transparência e tamanho novo
+    int width = 800;
+    int height = 1170;
+    int newHeight = height + 150;
+
+    BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
+
+    // copiar a imagem original pra nova imagem (em memória)
+    Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+    graphics.drawImage(originalImage, 0, 0, width, height, null);
+
+    // configurar a fonte
+    var font = new Font(Font.SANS_SERIF, Font.BOLD, 78);
+    graphics.setColor(Color.YELLOW);
+    graphics.setFont(font);
+
+    // escrever uma frase na nova imagem
+    graphics.drawString(subtitle, 100, newHeight - 50);
+
+    // escrever nova imagem num arquivo
+    ImageIO.write(newImage, "png", new File(fileName));
+
+    graphics.dispose();
+  }
+}
